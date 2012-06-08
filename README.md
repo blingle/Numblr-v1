9c292ec84f43ba1ac7b146dfda522a78f6c456dd
blingle <blingle@mail.usf.edu>
2012-06-08 13:54:55 -0400

    numblr-v1 commit:1

--- README.md
+++ README.md
@@ -1,4 +1,140 @@
 Numblr-v1
 =========
 
-Node.js interface that works with the Tumblr v2 api.  It can retrieve the tumblr avatar, tumblr blog info, and tumblr blog post/s.  This interface is small, succinct and works to move basic tumblr blog functionality to a person or professional website.
+  Author:  Brent Lingle
+  WebSite: http://www.brentisyourfriend.com
+  Contact: lingle.brent@gmail.com
+  Version: 1.0
+  Notes:   If you find any bugs, have questions or concerns, please
+           feel free to e-mail me!
+
+  This module provides an easy way to interact with tumblr's API 
+  using nodejs.  It is based on tumblr's API Version 2.  You may
+  have to adjust the file path, so that the 'http' module loads
+  properly.
+
+  This module provides basic functionality for requests to tumblr.  
+  You can retreive the Avatar url, the blog info, and blog posts 
+  in group or single format.
+
+  MIT Licensed
+  Tumblr API version: v2
+  Node.js Version: v0.6.17
+ 
+  You'll need 1 things:
+     1. Oauth key consumer key (api_key), provided by Tumblr
+
+  Visit the README.md file on github for the Documentation.
+
+  *** DISCLAIMER ***  THIS SOFTWARE IS 'AS-IS' 
+  
+  
+Methods
+
+//avatar method
+
+  numblr.avatar(hostName, size, cb)
+  
+    hostName: (required) The host name of the tumblr blog.
+      ex. "brentisyourfriend".tumblr.com'
+      brentisyourfried is the host name
+
+    size: (required) The size in pixels of the image.
+      Tumblr accepts these sizes 16, 24, 30, 40, 48, 
+      64, 96, 128, 512
+    
+    cb: (required) A callback function that is called after
+      the http request has been made to tumblr's api.  Will
+      return the data or an error.
+    
+    //return object
+    
+      This method returns the avatar url as a JSON object.
+        jsonObj.response.avatar_url
+
+//info method
+
+  numblr.info(hostName, apiKey, cb)
+
+    hostName: (required) The host name of the tumblr blog.
+      ex. "brentisyourfriend".tumblr.com'
+      brentisyourfried is the host name
+  
+    apiKey: (required) An Oauth key provided by tumblr.
+      You can visit tumblr and register for an Oauth key.
+      It takes only moments to register an application,
+      which is a host name.
+      
+    cb: (required) A callback function that is called after
+      the http request has been made to tumblr's api.  Will
+      return the data or an error.
+      
+    //return object
+    
+      This method returns the blog info as a json object.
+        jsonObj.response.blog.title //blog title
+        jsonObj.response.blog.posts //total # of posts
+        jsonObj.response.blog.name //name of blog
+        jsonObj.response.blog.url //url of blog
+        jsonObj.response.blog.updated //last updated
+        jsonObj.response.blog.description //blog description
+        jsonObj.response.blog.ask //boolean
+        jsonObj.response.blog.ask_anon //boolean
+        jsonObj.response.blog.likes //# of likes
+      
+//posts method
+
+  numblr.posts(hostName, apiKey, limit, offset, cb)
+  
+    hostName: (required) The host name of the tumblr blog.
+      ex. "brentisyourfriend".tumblr.com'
+      brentisyourfried is the host name
+
+    apiKey: (required) An Oauth key provided by tumblr.
+      You can visit tumblr and register for an Oauth key.
+      It takes only moments to register an application,
+      which is a host name.
+      
+    limit: (required) The total number of posts to retrieve.
+      The max limit is 20 posts at one time.
+      
+    offset: (required) The posts to start with.  0 is the
+      newest post.
+
+    cb: (required) A callback function that is called after
+      the http request has been made to tumblr's api.  Will
+      return the data or an error.
+
+      //return object
+
+        This method returns the blog posts as a json object.
+          This is a lengthy object, so I recommend looking 
+          at the tumblr documentation for a more detailed
+          description of this JSON object.
+          http://www.tumblr.com/docs/en/api/v2
+          
+          
+// Example on how to use this module
+
+var numblr = require('numblr');
+
+numblr.avatar('#someHost-Name', 512, function(err, data) {
+  if ( data ) {
+    //returned callback data
+  } else {
+   //error
+  }
+});
+          
+          
+          
+For more information please visit:
+http://www.tumblr.com/docs/en/api/v2
+          
+-or-
+
+contact me:
+
+Brent Lingle
+lingle.brent@gmail.com
+(changes and collaboration are welcome!)
---
+++ numblr.js
@@ -0,0 +1,190 @@
+/*
+  Author:  Brent Lingle
+  WebSite: http://www.brentisyourfriend.com
+  Contact: lingle.brent@gmail.com
+  Version: 1.0
+  Notes:   If you find any bugs, have questions or concerns, please
+           feel free to e-mail me!
+
+  *****************************************************************
+  This module provides an easy way to interact with tumblr's API 
+  using nodejs.  It is based on tumblr's API Version 2.  You may
+  have to adjust the file path, so that the 'http' module loads
+  properly.
+
+  This module provides basic functionality for requests to tumblr.  
+  You can retreive the Avatar url, the blog info, and blog posts 
+  in group or single format.
+
+  MIT Licensed
+  Tumblr API version: v2
+  Node.js Version: v0.6.17
+ 
+  You'll need 1 things:
+     1. Oauth key consumer key (api_key), provided by Tumblr
+
+  Visit the README.md file on github for the Documentation.
+
+  *** DISCLAIMER ***  THIS SOFTWARE IS 'AS-IS' 
+
+  *****************************************************************
+*/
+
+//required modules
+var http = require('http');
+
+//Returns the url of the tumblr Avatar.
+//The size must be ONE number, 16, 24, 30, 40, 48, 64
+//96, 128, 512 and is in pixels.
+exports.avatar = function(hostName, size, cb) {
+	
+	var obj = '';
+	
+	//if size isn't defined, or the inncorect type
+	//it defaults to 64 pixels
+	size = parseInt(size);
+	if (typeof size != "number") size = 64;
+	
+	
+	//create an options for http.get()
+	var options = {
+	  host: 'api.tumblr.com',
+	  port: 80,
+	  path: '/v2/blog/' + hostName + 
+	    '.tumblr.com/avatar/' + size 
+	};
+	
+	//make the request to tumblr
+	var req = http.get(options, function(res) {
+	  var data = '';
+	  
+	  //.on() event handler is used
+	  //to handle the response data
+	
+	  //collect data
+	  res.on('data', function(chunk) {
+		  data += chunk;
+	  });	
+    
+    //parse data	
+	  res.on('end', function(err) {
+		  try {
+			  obj = JSON.parse(data);
+			  cb(null, obj);
+		  } catch (err) {
+			  cb(err);
+		  }
+	  });
+	});
+};
+
+//This method returns the blog title, total number of posts,
+//the name of the blog, the time of the most recent
+//post in second since epoch, the blog description,
+//wether the blog allows questions, wether the blog
+//allows anonymous questions, and the number of 
+//likes for this user.  Return a JSON object.  
+exports.info = function(hostName, apiKey, cb) {
+  
+  var obj = '';
+  
+  //if no api_key is provided
+  //an error is returned
+  if (!apiKey) {
+    cb(err);
+  }
+  
+  //options for http.get()
+  var options = {
+    host: 'api.tumblr.com',
+    port: 80,
+    path: '/v2/blog/' + hostName + 
+        '.tumblr.com/info?api_key=' + apiKey 
+  };
+
+  //make the request to tumblr
+  var req = http.get(options, function(res) {
+	 
+    var data = '';
+	
+	  //.on() event handler is used
+	  //to handle the response data
+	
+    //collect the data
+    res.on('data', function(chunk) {
+      data += chunk;
+    });
+  
+    //parse the data
+	  res.on('end', function(err) {
+		  try {
+			  obj = JSON.parse(data);
+			  //console.log('The data was parsed');
+			  cb(null, obj);
+		  } catch (err) {
+			  //console.log('err0r');
+			  cb(err);
+		  }
+    });
+  });
+};
+
+//Returns the specified number of posts, up to 20 and the specified
+//post to start with.  Tumblr lists the first post with an Id of 0.
+//Every subsequent post is in ascending order. By specifying an offset
+//that is the first post that will be selected.  Each post will be
+//chosen in numerical ascending order.  Return a JSON object.
+exports.posts = function(hostName, apiKey, limit, offset, cb){
+ 
+  var obj = '';
+
+  //check the api_key
+  if ( !apiKey ) {
+    cb(err);
+  }
+
+  //check limit type
+  limit = parseInt(limit);
+  if ( typeof limit !== "number" ) limit = 20;
+   
+  //check offset type
+  offset = parseInt(offset);
+  if ( typeof offset !== "number" ) offset = 0;  
+  
+  //options for http.get()
+  var options = {
+    host: 'api.tumblr.com',
+    port: 80,
+    path: '/v2/blog/' + hostName + 
+        '.tumblr.com/posts?api_key=' + apiKey +
+        '&' + 'limit=' + limit  + '&offset=' + 
+        offset,
+  };
+  
+  //Check the path that is being sent to tumblr
+  //console.log(options.path);
+
+  //make the request to tumblr
+  var req = http.get(options, function(res) {
+	 
+    var data = '';
+	
+	  //.on() event handler is used
+	  //to handle the response data
+	
+    //collect the data
+    res.on('data', function(chunk) {
+      data += chunk;
+    });
+  
+    //parse the data
+	  res.on('end', function(err) {
+		  try {
+			  obj = JSON.parse(data);
+			  cb(null, obj);
+		  } catch (err) {
+			  cb(err);
+		  }
+    });
+  });	
+};
